6abcbe1b6574b2677abe6683e5209aecbb76c542
kitkatmia <alinka.markova@gmail.com>
2022-07-19 12:13:44 -0700

    Initial Commit

---
+++ FIA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FIA
+//
+//  Created by Alex Markova on 7/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
