43c2db57e8af70f2eaeb3cf11d245db8fc62f90e
kitkatmia <alinka.markova@gmail.com>
2022-07-21 14:39:25 -0700

    Added outlets and actions for timeline

---
+++ FIA/TimelineController.swift
@@ -0,0 +1,83 @@
+//
+//  TimelineController.swift
+//  FIA
+//
+//  Created by Alex Markova on 7/21/22.
+//
+
+import UIKit
+
+class TimelineController: UIViewController {
+    @IBOutlet weak var mid1800s: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var thirdLabel: UILabel!
+    @IBOutlet weak var fourthLabel: UILabel!
+    @IBOutlet weak var fifthLabel: UILabel!
+    @IBOutlet weak var sixthLabel: UILabel!
+    @IBOutlet weak var seventhLabel: UILabel!
+    @IBOutlet weak var eighthLabel: UILabel!
+    @IBOutlet weak var ninthLabel: UILabel!
+    @IBOutlet weak var tenthLabel: UILabel!
+    @IBOutlet weak var twelvthLabel: UILabel!
+    @IBOutlet weak var lastLabel: UILabel!
+    @IBOutlet weak var eleventhLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func clickedMid1800s(_ sender: Any) {
+    }
+    
+    @IBAction func clicked1860(_ sender: Any) {
+    }
+    
+    @IBAction func clicked1910(_ sender: Any) {
+    }
+    
+    @IBAction func clicked1930(_ sender: Any) {
+    }
+    
+    @IBAction func clicked1940(_ sender: Any) {
+    }
+    
+    @IBAction func clicked1950(_ sender: Any) {
+    }
+    
+    @IBAction func clicked1965(_ sender: Any) {
+    }
+    
+    @IBAction func clicked1973(_ sender: Any) {
+    }
+    
+    @IBAction func clicked1976(_ sender: Any) {
+    }
+    
+    @IBAction func clicked1982(_ sender: Any) {
+    }
+    
+    @IBAction func clicked2017(_ sender: Any) {
+    }
+    
+    @IBAction func clicked2019(_ sender: Any) {
+    }
+    
+    @IBAction func clicked2022(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
