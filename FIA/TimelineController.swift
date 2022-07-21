2b4ebd9caea4f2a9cf6794e716cbcf686df41b85
kitkatmia <alinka.markova@gmail.com>
2022-07-21 14:47:14 -0700

    added show/hide commands on timeline

--- FIA/TimelineController.swift
+++ FIA/TimelineController.swift
@@ -24,46 +24,100 @@ class TimelineController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mid1800s.isHidden = true
+        secondLabel.isHidden = true
+        thirdLabel.isHidden = true
+        fourthLabel.isHidden = true
+        fifthLabel.isHidden = true
+        sixthLabel.isHidden = true
+        seventhLabel.isHidden = true
+        eighthLabel.isHidden = true
+        ninthLabel.isHidden = true
+        tenthLabel.isHidden = true
+        eleventhLabel.isHidden = true
+        twelvthLabel.isHidden = true
+        lastLabel.isHidden = true
 
         // Do any additional setup after loading the view.
     }
+    func hideAllText() {
+        mid1800s.isHidden = true
+        secondLabel.isHidden = true
+        thirdLabel.isHidden = true
+        fourthLabel.isHidden = true
+        fifthLabel.isHidden = true
+        sixthLabel.isHidden = true
+        seventhLabel.isHidden = true
+        eighthLabel.isHidden = true
+        ninthLabel.isHidden = true
+        tenthLabel.isHidden = true
+        eleventhLabel.isHidden = true
+        twelvthLabel.isHidden = true
+        lastLabel.isHidden = true
+    }
     @IBAction func clickedMid1800s(_ sender: Any) {
+        hideAllText()
+        mid1800s.isHidden = false
     }
     
     @IBAction func clicked1860(_ sender: Any) {
+        hideAllText()
+        secondLabel.isHidden = false
     }
     
     @IBAction func clicked1910(_ sender: Any) {
+        hideAllText()
+        thirdLabel.isHidden = false
     }
     
     @IBAction func clicked1930(_ sender: Any) {
+        hideAllText()
+        fourthLabel.isHidden = false
     }
     
     @IBAction func clicked1940(_ sender: Any) {
+        hideAllText()
+        fifthLabel.isHidden = false
     }
     
     @IBAction func clicked1950(_ sender: Any) {
+        hideAllText()
+        sixthLabel.isHidden = false
     }
     
     @IBAction func clicked1965(_ sender: Any) {
+        hideAllText()
+        seventhLabel.isHidden = false
     }
     
     @IBAction func clicked1973(_ sender: Any) {
+        hideAllText()
+        eighthLabel.isHidden = false
     }
     
     @IBAction func clicked1976(_ sender: Any) {
+        hideAllText()
+        ninthLabel.isHidden = false
     }
     
     @IBAction func clicked1982(_ sender: Any) {
+        hideAllText()
+        tenthLabel.isHidden = false
     }
     
     @IBAction func clicked2017(_ sender: Any) {
+        hideAllText()
+        eleventhLabel.isHidden = false
     }
     
     @IBAction func clicked2019(_ sender: Any) {
+        hideAllText()
+        twelvthLabel.isHidden = false
     }
     
     @IBAction func clicked2022(_ sender: Any) {
+        hideAllText()
+        lastLabel.isHidden = false
     }
     
     
